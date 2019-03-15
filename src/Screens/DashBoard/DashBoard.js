4723770c7b7e0e01ad5b3ee65df99b0e3b421da9
Chek_Fire <chekkoriamine123@gmail.com>
2019-03-15 12:12:35 +0100

    Commit for pull

--- src/Screens/DashBoard/DashBoard.js
+++ src/Screens/DashBoard/DashBoard.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
-import DashboardMenu from '../../components/DashboardMenu/DashBoardMenu';
+import DashboardMenu from '../../components/dashboardMenu/DashboardMenu';
 
 export default class DashBoard extends Component {
   render() {
     return (
       <div className="container">
          <div style={{width:'100%'}}>
-            <DashboardMenu />
+          <DashboardMenu />
         </div>  
       </div>
     )
